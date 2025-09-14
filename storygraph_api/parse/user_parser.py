b3b4a5eb4f29bba52c4eded6312f093a9fd63e46
wavdl <bvanderlaar@proton.me>
2025-09-13 21:50:59 -0400

    Add function for getting "Up Next" books from a user's "To Read" list.

--- storygraph_api/parse/user_parser.py
+++ storygraph_api/parse/user_parser.py
@@ -5,8 +5,10 @@ from bs4 import BeautifulSoup
 class UserParser:
     @staticmethod
     @parsing_exception 
-    def parse_html(html):
+    def parse_html(html, id_enclosure=None):
         soup = BeautifulSoup(html, 'html.parser')
+        if id_enclosure:
+            soup = soup.find_all('div', attrs={"id": id_enclosure})[0]
         books_list = []
         books = soup.find_all('div', class_="book-title-author-and-series")
         for book in books:
@@ -28,6 +30,11 @@ class UserParser:
     def to_read(uname, cookie):
         content = UserScraper.to_read(uname,cookie)
         return UserParser.parse_html(content)
+
+    @staticmethod
+    def up_next(uname, cookie):
+        content = UserScraper.to_read(uname, cookie)
+        return UserParser.parse_html(content, "up-next-section")
 
     @staticmethod
     def books_read(uname, cookie):
